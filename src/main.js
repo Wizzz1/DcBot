af6a12818310745765f838811993e5955b2dd040
Wizzz1 <126481855+Wizzz1@users.noreply.github.com>
2024-04-13 20:15:42 +0800

    commit scripts
    scripts: dev

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
 import dotenv from 'dotenv'
 
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 dotenv.config()
