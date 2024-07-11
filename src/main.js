07998b241d87daca637ee3ec061d00964b2052c6
Wizzz1 <126481855+Wizzz1@users.noreply.github.com>
2024-07-12 01:20:12 +0800

    EP4
    vite-node, vue, pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
+import vueInit from '@/core/vue'
 import dotenv from 'dotenv'
+
+vueInit()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
